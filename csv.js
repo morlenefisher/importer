300c39b90e18a919cfc8144e10766331678ef6b4
Morlene Fisher <morlene@ashleylawrence.com>
2016-12-03 14:11:05 +0000

    converted csv to cli tool

--- csv.js
+++ csv.js
@@ -1,19 +1,46 @@
-var parse = require('csv-parse');
-var fs = require('fs');
+#!/usr/bin/env node
 
-let file = fs.readFileSync(process.cwd() + "/sample.csv");
-if (typeof file === 'undefined') throw new Error('Cannot find the data file');
 
-let sfile = fs.readFileSync(process.cwd() + "/dp.schema.json");
-if (typeof sfile === 'undefined') throw new Error('Cannot find the schema file');
-let schema = JSON.parse(sfile);
+(function () {
 
-const schema_props = Object.keys(schema.properties);
-console.log(schema_props);
+    let generator = {};
 
-parse(file, {
-    columns: schema_props
-}, function(err, output) {
-    console.log(err);
-    console.log(output);
-});
+    exports.generate = generator.go = () => {
+        let args = process.argv.slice(2);
+
+        let source = args[0];
+        let schemas = args[1]
+
+        console.log(source);
+        console.log(schemas);
+
+        let parse = require('csv-parse');
+        let fs = require('fs');
+
+        let file = fs.readFileSync(process.cwd() + '/' + source);
+        if (typeof file === 'undefined') throw new Error('Cannot find the data file');
+
+        let sfile = fs.readFileSync(process.cwd() + '/' + schemas);
+        if (typeof sfile === 'undefined') throw new Error('Cannot find the schema file');
+        let schema = JSON.parse(sfile);
+
+        const schema_props = Object.keys(schema.properties);
+        console.log(schema_props);
+
+        parse(file, {
+            columns: schema_props
+        }, function (err, output) {
+                if (err) throw err;
+            // console.log(output);
+
+            fs.writeFile('sample.json', JSON.stringify(output), (err) => {
+                if (err) throw err;
+                console.log('It\'s saved!');
+            });
+        });
+    }
+
+    if (!module.parent) {
+        generator.go();
+    }
+})();
